34ab6152d0dc59822fb08046143567d4f5bb792b
bhupendrapt95 <bhupendrapt95@gmail.com>
2020-11-26 02:54:47 +0530

    added contants and API-key

---
+++ lib/api/news_api.dart
@@ -0,0 +1,9 @@
+class NewsApi {
+  final Map<String, String> header = {
+    'Content-Type': 'application/json',
+    //API key [https://newsapi.org]
+    'X-Api-Key': '9098215d6ffd4b78ab714da739f558cc',
+  };
+
+  final String baseURL = 'https://newsapi.org/v2/';
+}
---
+++ lib/constantans.dart
@@ -0,0 +1,13 @@
+const Map<String, String> sources = {
+  'CNN': 'cnn',
+  'CBS News': 'cbs-news',
+  'Buzzfeed': 'buzzfeed',
+  'ESPN': 'espn',
+  'Fox News': 'fox-news',
+  'MSNBC': 'msnbc',
+  'AP': 'associated-press',
+  'Time': 'time',
+  'The Times of India"': 'the-times-of-india',
+  'The Hindu': 'the-hindu',
+  'Google News (IN)': 'google-news-in',
+};
